9ae70e47b4393aaeb4cbb8ced5e87020db25a54e
zero <35671110+zeroclutch@users.noreply.github.com>
2019-09-17 17:38:05 -0700

    Text fixes.

--- commands/economy/daily.js
+++ commands/economy/daily.js
@@ -26,7 +26,7 @@ module.exports = {
         const collection = msg.client.database.collection('users')
         const characters = ['<:gamebot_g:623660149470527510>', '<:gamebot_a:623660148992245761>', '<:gamebot_m:623660148682129421>', '<:gamebot_e:623660148950302721>', '<:gamebot_b:623660148690255873>', '<:gamebot_o:623660148891582505>', '<:gamebot_t:623660148858290187>']
         msg.author.fetchDBInfo().then(async info => {
-            const voteStreak = streak => `**${streak} day streak!**\n${characters.slice(0, streak)}${'⬜'.repeat(Math.max(7 - streak, 0))} | **Next Reward: ${DAILY_REWARDS[Math.min(streak, 7)]}${options.creditIcon}**`
+            const voteStreak = streak => `**${streak} day streak!**\n${characters.slice(0, streak).join()}${'⬜'.repeat(Math.max(7 - streak, 0))} | **Next Reward: ${DAILY_REWARDS[Math.min(streak, 7)]}${options.creditIcon}**`
 
             // check if user can claim credits
             // daily hasn't been claimed and check if lastVoted was in last 12 hours
