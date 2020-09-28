8e2f978e5fd4f387fb0740eed0b1863446ee3761
zero <35671110+zeroclutch@users.noreply.github.com>
2020-09-28 11:57:04 -0700

    Added Lichess integration

---
+++ games/Chess/classes/LichessAPI.js
@@ -0,0 +1,23 @@
+const request = require('axios')
+
+/**
+ * An API wrapper that allows for basic interfacing with the Lichess API.
+ * @class
+ */
+const LichessAPI = class LichessAPI {
+    constructor() {
+    }
+
+    async importGame(PGN) {
+        return await request({
+            method: 'post',
+            url: 'https://lichess.org/api/import',
+            data: PGN,
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        })
+    }
+}
+
+module.exports = LichessAPI
