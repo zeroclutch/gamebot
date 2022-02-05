7b9f1ffa68fa0081a42d2afe4bf26496b41e613b
zero <35671110+zeroclutch@users.noreply.github.com>
2022-02-05 14:47:17 -0800

    Add error handlers to all process events

--- events/process/unhandledRejection.js
+++ events/process/unhandledRejection.js
@@ -2,8 +2,15 @@ import logger from 'gamebot/logger'
 export const eventName = 'unhandledRejection'
 
 export const handler = (reason, promise) => {
-    logger.error(reason)
-    promise.catch(err => {
-        logger.error(err)
-    })
+    try {
+        logger.error(reason)
+        promise.catch(err => {
+            logger.error(err)
+        })
+    } catch(err2) {
+        console.error('There was an error handling the error.')
+        console.error('Initial error', err)
+        console.error('Logger error', err2)
+    }
+
 }
