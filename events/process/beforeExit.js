321437567eae3b00e5b9346aeee077ef97a78fc8
zero <35671110+zeroclutch@users.noreply.github.com>
2022-02-05 14:47:17 -0800

    Add error handlers to all process events

--- events/process/beforeExit.js
+++ events/process/beforeExit.js
@@ -2,5 +2,11 @@ import logger from 'gamebot/logger'
 export const eventName = 'beforeExit'
 
 export const handler = code => {
-    logger.fatal(`Process ${process.pid} exiting with code ${code}`)
+    try {
+        logger.fatal(`Process ${process.pid} exiting with code ${code}`)
+    } catch(err2) {
+        console.error('There was an error handling the error.')
+        console.error('Initial error', err)
+        console.error('Logger error', err2)
+    }
 }
--- events/process/uncaughtException.js
+++ events/process/uncaughtException.js
@@ -2,8 +2,14 @@ import logger from 'gamebot/logger'
 export const eventName = 'uncaughtException'
 
 export const handler = (err, origin) => {
-    logger.fatal(err)
-    logger.fatal(origin)
+    try {
+        logger.fatal(err)
+        logger.fatal(origin)
+    } catch(err2) {
+        console.error('There was an error handling the error.')
+        console.error('Initial error', err)
+        console.error('Logger error', err2)
+    }
 
     process.exit(1)
 }
