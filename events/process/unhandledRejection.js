5c1c13241729c7531735d514ece01adc96f7e3cf
zero <35671110+zeroclutch@users.noreply.github.com>
2022-03-08 17:51:13 -0800

    Fix infinite error loop

--- events/process/unhandledRejection.js
+++ events/process/unhandledRejection.js
@@ -3,14 +3,10 @@ export const eventName = 'unhandledRejection'
 
 export const handler = (reason, promise) => {
     try {
-        logger.error(reason)
-        promise.catch(err => {
-            logger.error(err)
-        })
+        logger.error(reason, promise)
+        promise.catch(logger.error).catch(console.error)
     } catch(err2) {
         console.error('There was an error handling the error.')
-        console.error('Initial error', err)
         console.error('Logger error', err2)
     }
-
 }
