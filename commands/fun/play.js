f5804aa96c2b755ce797e3c77195fc19f9859127
zero <35671110+zeroclutch@users.noreply.github.com>
2022-11-02 00:06:15 -0700

    Minor text fixes

--- commands/fun/play.js
+++ commands/fun/play.js
@@ -3,25 +3,7 @@ import options from './../../config/options.js'
 import Discord from 'discord.js-light'
 const { Constants, CommandInteraction } = Discord
 
-import fs from 'node:fs'
-import path from 'node:path'
-
-let __dirname = path.dirname(new URL(import.meta.url).pathname) // Get the directory name of the current file
-
-// Read all the games from the games folder
-const games = fs.readdirSync(decodeURIComponent(path.join(__dirname, '..', '..', 'games'))).filter(
-  game => (game !== '.DS_Store' && !game.startsWith('_'))
-)
-
-// Read metadata from each game
-const choices = (() => games.map(async game => {
-    const { default: metadata } = await import(`../../games/${game}/metadata.js`)
-
-    return {
-      name: metadata.name,
-      value: metadata.id,
-    }
-}))();
+import { choices } from '../../types/util/games.js'
 
 import BotCommand from '../../types/command/BotCommand.js'
 export default new BotCommand({
