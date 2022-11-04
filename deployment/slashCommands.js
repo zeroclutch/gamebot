1d8f332f6d9330ec69292b0804affb8708a93b27
zero <35671110+zeroclutch@users.noreply.github.com>
2022-11-03 19:20:35 -0700

    Update giveaway command

--- deployment/slashCommands.js
+++ deployment/slashCommands.js
@@ -2,11 +2,27 @@ import { REST } from '@discordjs/rest';
 import { Routes } from 'discord-api-types/v10';
 
 import logger from 'gamebot/logger'
+import assert from 'node:assert';
 
 import fs from 'node:fs';
 import path from 'node:path';
 
 const GUILD_ID = '777699407876849684' // Server guild ID
+
+const TARGETS = Object.freeze({
+	GUILD: 'guild',
+	GLOBAL: 'global',
+})
+
+let MODE = TARGETS.GUILD
+
+// Read process arguments
+let targetArg = process.argv.find(arg => arg.startsWith('--target='))
+console.log(process.argv)
+if(targetArg) {
+	MODE = targetArg.replace('--target=', '')
+	assert.strictEqual(MODE === TARGETS.GUILD || MODE === TARGETS.GLOBAL, true, 'Invalid target specified.')
+}
 
 const __dirname = path.dirname(new URL(import.meta.url).pathname) // Get the directory name of the current file
 const BASE_DIR = path.join(__dirname, '..') // Get the directory name of the current file
@@ -37,13 +53,23 @@ const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_BOT_TOKEN)
 	try {
 		console.log(`Started refreshing ${commands.length} application (/) commands.`);
 
-		// The put method is used to fully refresh all commands in the guild with the current set
-		const data = await rest.put(
-			Routes.applicationGuildCommands(process.env.DISCORD_CLIENT_ID, GUILD_ID),
-			{ body: commands },
-		);
+		let response = []
 
-		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+		if(MODE === TARGETS.GUILD) {
+			// The put method is used to fully refresh all commands in the guild with the current set
+			response = await rest.put(
+				Routes.applicationGuildCommands(process.env.DISCORD_CLIENT_ID, GUILD_ID),
+				{ body: commands },
+			);
+		} else if(MODE === TARGETS.GLOBAL) {
+			// Refresh all commands in the global scope
+			response = await rest.put(
+				Routes.applicationCommands(process.env.DISCORD_CLIENT_ID),
+				{ body: commands },
+			);
+		}
+
+		console.log(`Successfully reloaded ${response.length} application (/) commands.`);
 	} catch (error) {
 		// And of course, make sure you catch and log any errors!
 		console.error(error);
