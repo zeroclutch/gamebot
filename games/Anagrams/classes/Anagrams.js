8d787753f981acea3c2890951d893c3709125bf6
zero <35671110+zeroclutch@users.noreply.github.com>
2024-05-21 19:21:32 -0700

    Make pangrams easier to find

--- games/Anagrams/classes/Anagrams.js
+++ games/Anagrams/classes/Anagrams.js
@@ -5,23 +5,25 @@ import logger from 'gamebot/logger'
 import fs from 'fs'
 import { time } from 'discord.js'
 
-const createDictionary = (path) => {
-    let words = fs.readFileSync(path, { encoding: 'utf-8' }, err => {
-        logger.error(err)
-    })
-    
-    let dict = {}
-    
-    words = words.split('\n')
-    words.splice(0,1)
-    
-    words.forEach(word => dict[word] = true)
-    return dict
-}
+// Define words from dictionary
+let words = fs.readFileSync('./gameData/WordGames/Collins_Scrabble_Dictionary.txt', { encoding: 'utf-8' }, err => {
+    logger.error(err)
+})
 
-const wordRegistry = createDictionary('./gameData/WordGames/Collins_Scrabble_Dictionary.txt')
-const commonWordRegistry = createDictionary('./gameData/WordGames/Common_Words.txt')
+let wordRegistry = {}
 
+words = words.split('\n')
+words.splice(0,1)
+
+words.forEach(word => wordRegistry[word] = true)
+
+// Define common words
+let commonWords = fs.readFileSync('./gameData/WordGames/Common_Words.txt', { encoding: 'utf-8' }, err => {
+    logger.error(err)
+})
+
+commonWords = commonWords.split('\n')
+commonWords.splice(0,1)
 
 export default class Anagrams extends Game {
     constructor(msg, settings) {
@@ -85,7 +87,7 @@ export default class Anagrams extends Game {
      * @param {number} length The length of the word.
      */
     chooseWord(length) {
-        const validWords = words.filter(w => w.length == length)
+        const validWords = commonWords.filter(w => w.length == length)
         const index = Math.floor(Math.random()*validWords.length)
         this.pangram = validWords[index]
         this.word = this.scramble(validWords[index])
@@ -200,72 +202,9 @@ export default class Anagrams extends Game {
         })
     }
 
-    /**
-     * Starts a new Anagrams game in DMs
-     */
-    async playDMs(callback) {
-        if(this.options['Custom Word'] == 'none') {
-            this.chooseWord(7)
-        } else {
-            this.word = this.options['Custom Word'].toUpperCase()
-        }
-        this.channel.sendEmbed('The game will start in 5 seconds. Check your direct messages to see the letters you have to unscramble!')
-        this.players.forEach(player => {
-            // Alert each user the game is starting
-            player.dmChannel.send({
-                embeds: [{
-                    title: 'Anagrams',
-                    description: `To earn points, make words using the letters below and send them in this channel. You have 60 seconds to make as many words as possible.\n\n**The letters are: \`Loading...\`**`,
-                    color: options.colors.info
-                }]
-            }).then(async message => {
-                // Wait 5 seconds before start
-                await this.sleep(5000)
-                let gameEndTime = Date.now() + 60000
-                message.edit({
-                    embeds: [{
-                        title: 'Anagrams',
-                        description: `To earn points, make words using the letters below and send them in this channel. Time expires ${time(Math.round(gameEndTime / 1000), 'R')}\n\n**The letters are: ${this.emojify(this.word)}**`,
-                        color: options.colors.info
-                    }]
-                })
-                // create a collector on each DM channel
-                 //this.validateWord(m.content, this.word) && !player.words.includes(word)
-                //console.log(player.dmChannel)
-                player.user.createDM().then(channel => {
-                    const filter = m => {
-                        return true
-                    }
-                    let collector = channel.createMessageCollector({ filter, time: 15000 })
-                    collector.on('collect', m => console.log(`Collected ${m.content}`))
-                    collector.on('end', collected => console.log(`Collected ${collected.size} items`));
-                    
-                })
-                /* collector.on('collect', (message) => {
-                    let word = message.content.toUpperCase()
-                    let score = this.getWordScore(word)
-                    player.dmChannel.send({
-                        embeds: [{
-                            description: `You got **${word}** for **${score}** points.\n\nThe letters are: \`${this.word}\``,
-                            title: isPangram(word) ? 'PANGRAM!' : '', 
-                            color: isPangram(word) ? options.colors.economy : options.colors.info
-                        }]
-                    })
-                    player.score += this.getWordScore(word)
-                    player.words.push(word)
-                })*/
-                /*player.collector.on('end', (collected) => {
-                    callback()
-                })*/
-            }).catch(logger.error.bind(logger))
-        })
-    }
-
     play() {
         if(this.options['Game Mode'] == 'Frenzy') {
             this.playFrenzy(() => {this.finish()})
-        } else if (this.options['Game Mode'] == 'DMs') {
-            this.playDMs(() => {this.finish()})
         }
     }
 
@@ -298,6 +237,4 @@ export default class Anagrams extends Game {
             this.end(winner)
         })
     }
-    
-
 }
