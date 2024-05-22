6e17c2dc729dd13d5ba57517272d95a09f45d3a3
zero <35671110+zeroclutch@users.noreply.github.com>
2024-05-21 18:16:05 -0700

    Fix bug where lichess links could show as undefined

--- games/Chess/classes/LichessAPI.js
+++ games/Chess/classes/LichessAPI.js
@@ -8,8 +8,8 @@ const LichessAPI = class LichessAPI {
     constructor() {
     }
 
-    async importGame(PGN) {
-        return await request({
+    importGame(PGN) {
+        return request({
             method: 'post',
             url: 'https://lichess.org/api/import',
             data: PGN,
